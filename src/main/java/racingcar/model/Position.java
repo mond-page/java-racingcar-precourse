7be3d01569d67aa228bbcec15c0db8f03146b1d7
mond <revive29@gmail.com>
2022-04-20 02:43:58 +0900

    feat(Position): 전진 가능성에 따른 움직임 추가
    - 전진 가능성에 따른 moveStep 메소드 추가
    - 해당 위치값을 반환하는 getPosition 메소드 추가
    - 생성자 대신 정적 팩토리 메소드 create 추가
    - 외부 접근이 필요 없는 메소드(isMoving, validateNumberRange) public에서 default 접근제어자 변경
    - 테스트 코드 ParameterizedTest로 리팩토링

--- src/main/java/racingcar/model/Position.java
+++ src/main/java/racingcar/model/Position.java
@@ -6,15 +6,36 @@ public class Position {
     private static final int MIN_NUMBER = 0;
     private static final int MAX_NUMBER = 9;
     private static final int MOVING_MIN_NUMBER = 4;
+    private static final int START_POSITION_VALUE = 0;
 
-    public static boolean isMoving(int number) {
+    private int value;
+
+    private Position() {
+        value = START_POSITION_VALUE;
+    }
+
+    public static Position create() {
+        return new Position();
+    }
+
+    public void moveStep(int number) {
+        if (isMoving(number)) {
+            this.value += 1;
+        }
+    }
+
+    boolean isMoving(int number) {
         validateNumberRange(number);
         return number >= MOVING_MIN_NUMBER;
     }
 
-    static void validateNumberRange(int number) {
+    void validateNumberRange(int number) {
         if (number < MIN_NUMBER || number > MAX_NUMBER) {
             throw new IllegalArgumentException(PositionMessage.INVALID_NUMBER_RANGE_MESSAGE);
         }
+    }
+
+    public int getPosition() {
+        return value;
     }
 }
--- src/test/java/racingcar/model/PositionTest.java
+++ src/test/java/racingcar/model/PositionTest.java
@@ -2,42 +2,51 @@ package racingcar.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertAll;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.constants.PositionMessage;
 
 class PositionTest {
+    private Position position;
 
-    @Test
-    @DisplayName("4이상 9이하의 수이면 전진할 수 있다")
-    void canMove() {
-        assertAll(
-                () -> assertThat(Position.isMoving(4)).isTrue(),
-                () -> assertThat(Position.isMoving(9)).isTrue()
-        );
+    @BeforeEach
+    void setUp() {
+        position = Position.create();
     }
 
-    @Test
-    @DisplayName("0이상 3이하의 수이면 전진할 수 없다")
-    void canNotMove() {
-        assertAll(
-                () -> assertThat(Position.isMoving(3)).isFalse(),
-                () -> assertThat(Position.isMoving(0)).isFalse()
-        );
+    @ParameterizedTest(name = "4이상 9이하의 수({0})이면 전진할 수 있다")
+    @ValueSource(ints = {4, 9})
+    void canMove(int number) {
+        assertThat(position.isMoving(number)).isTrue();
     }
 
-    @Test
-    @DisplayName("0보다 작거나 9보다 큰 수는 잘못된 값이다")
-    void invalidNumber() {
-        assertAll(
-                () -> assertThatThrownBy(() -> Position.validateNumberRange(10))
-                        .isInstanceOf(IllegalArgumentException.class)
-                        .hasMessageContaining(PositionMessage.INVALID_NUMBER_RANGE_MESSAGE),
-                () -> assertThatThrownBy(() -> Position.validateNumberRange(-1))
-                        .isInstanceOf(IllegalArgumentException.class)
-                        .hasMessageContaining(PositionMessage.INVALID_NUMBER_RANGE_MESSAGE)
-        );
+    @ParameterizedTest(name = "0이상 3이하의 수({0})이면 전진할 수 없다")
+    @ValueSource(ints = {0, 3})
+    void canNotMove(int number) {
+        assertThat(position.isMoving(number)).isFalse();
+    }
+
+    @ParameterizedTest(name = "0보다 작거나 9보다 큰 수({0})는 잘못된 값이다")
+    @ValueSource(ints = {-1, 10})
+    void invalidNumber(int number) {
+        assertThatThrownBy(() -> position.validateNumberRange(number))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(PositionMessage.INVALID_NUMBER_RANGE_MESSAGE);
+    }
+
+    @ParameterizedTest(name = "전진이 가능(입력받은 수 : {0})하다면 1만큼 움직인다")
+    @ValueSource(ints = {4, 9})
+    void isMovingTrueThenMoveStep(int number) {
+        position.moveStep(number);
+        assertThat(position.getPosition()).isEqualTo(1);
+    }
+
+    @ParameterizedTest(name = "전진이 불가능(입력받은 수 : {0})하다면 움직이지 않는다")
+    @ValueSource(ints = {0, 3})
+    void isMovingFalseThenDontMoveStep(int number) {
+        position.moveStep(number);
+        assertThat(position.getPosition()).isZero();
     }
 }
