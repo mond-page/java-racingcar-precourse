c6a9c15a3810685cba92fe97b02598ea51ac5a73
mond <revive29@gmail.com>
2022-04-21 01:26:34 +0900

    fix(NameMessage): 불필요한 생성자 제한
    - 생성자의 접근제어자를 public에서 private로 변경

--- src/main/java/racingcar/constants/NameMessage.java
+++ src/main/java/racingcar/constants/NameMessage.java
@@ -2,4 +2,7 @@ package racingcar.constants;
 
 public final class NameMessage {
     public static final String INVALID_NAME_LENGTH = "[ERROR] 이름의 길이는 1에서 5자리입니다.";
+
+    private NameMessage() {
+    }
 }
