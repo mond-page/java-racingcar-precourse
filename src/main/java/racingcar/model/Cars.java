f57fdecc910e37bc85dd31c23055117896f3d063
mond <revive29@gmail.com>
2022-04-21 22:13:53 +0900

    feat(Cars): 자동차들에게 0에서 9사이의 랜덤값을 부여
    - 자동차들에게 랜덤값을 부여하는 move 메소드 추가

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -1,10 +1,11 @@
 package racingcar.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Cars {
-    private List<Car> cars;
+    private final List<Car> cars;
 
     private Cars(Names names) {
         cars = new ArrayList<>();
@@ -15,6 +16,12 @@ public class Cars {
 
     public static Cars create(Names names) {
         return new Cars(names);
+    }
+
+    public void move() {
+        for (Car car : cars) {
+            car.move(Randoms.pickNumberInRange(0, 9));
+        }
     }
 
     public List<Car> getCars() {
