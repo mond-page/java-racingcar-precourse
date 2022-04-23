5f559ca5e55e70f7d81ef2ae50ff6dbcac085866
mond <revive29@gmail.com>
2022-04-23 22:31:16 +0900

    refactor(Receipt): 상수명 Postfix 추가
    - 메세지 성격의 상수는 Postfix "MESSAGE" 추가

--- src/main/java/racingcar/view/Receipt.java
+++ src/main/java/racingcar/view/Receipt.java
@@ -1,18 +1,18 @@
 package racingcar.view;
 
 public final class Receipt {
-    public static final String INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    public static final String INPUT_REPEAT_NUMBER = "시도할 회수는 몇회인가요?";
+    public static final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String INPUT_REPEAT_NUMBER_MESSAGE = "시도할 회수는 몇회인가요?";
 
     private Receipt() {
     }
 
     public static void printInputCarNames() {
-        System.out.println(INPUT_CAR_NAMES);
+        System.out.println(INPUT_CAR_NAMES_MESSAGE);
     }
 
     public static void printInputRepeatNumber() {
-        System.out.println(INPUT_REPEAT_NUMBER);
+        System.out.println(INPUT_REPEAT_NUMBER_MESSAGE);
     }
 
     public static void printErrorMessage(IllegalArgumentException e) {
